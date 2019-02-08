0de632ae8abbd31d018012777a02fa77a433fcb5
Soret Brandon <brandonsoret@hotmail.fr>
2019-02-08 14:10:39 +0100

    Soldat Animé
    Propre

--- www/sprite.js
+++ www/sprite.js
@@ -3,11 +3,6 @@ let context = document.getElementsByTagName("canvas")[0].getContext("2d");
 
 //FUNCTIONS  ----------------------------------------------------------
 
-//TILEMAP
-
-
-
-
 //RECUPERER LES LIGNES D'ENTREE
 function input()
 	{
@@ -23,7 +18,13 @@ function animate()
 // AFFICHER A L'ECRAN LES ELEMENTS
 function render()
 	{
-		
+		CreateRect(0,0,caneva.width,caneva.height,'white');
+
+		for (sprite of ArmeeDeSprite) {
+			
+			sprite.DrawSprite();
+			sprite.AnimateSprite();
+		}
 	}
 
 function GameLoop()
@@ -37,62 +38,63 @@ function GameLoop()
 		window.requestAnimationFrame(GameLoop);
 	}
 
-//POUR LES SOLDATS
-function CreateCarre()
+//TILEMAP
+
+//CREATION DE SOLDATS
+function CreateSprite(img, dx, dy){
+	let sprite = {	positionCaneva : {x : dx, y : dy},
+					positionImage: {x : 144, y : 108},
+					tailleCaneva : {width : 32 , height : 42},
+					tailleImage : {width : 15 , height : 22},
+					vitesse : {x : 3, y : 3},
+					direction : {x : 1, y : 1},
+					image : img,
+					//Pour faire varier les images
+					step : 0.1,
+					DrawSprite : function()
+					{
+						//void ctx.drawImage(image, sx, sy, sLargeur, sHauteur, dx, dy, dLargeur, dHauteur);
+						context.drawImage(sprite.image, sprite.positionImage.x + 16 * Math.round(sprite.step), sprite.positionImage.y, sprite.tailleImage.width, sprite.tailleImage.height, sprite.positionCaneva.x, sprite.positionCaneva.y, sprite.tailleCaneva.width, sprite.tailleCaneva.height);
+					},
+					AnimateSprite : function()
+					{
+						if(sprite.step > 3)
+						{
+							sprite.step = 0.1
+						}
+						sprite.step += 0.1;
+					}
+
+				};
+
+	return sprite;
+}
+
+//CHARGER LES SPRITES SHEETS
+function LoadSpriteSheets()
 	{
-		
+		let SpriteSheets = [],
+		SpriteImages;
+
+		for (var i = 0; i < 12; i++) {
+			SpriteImages = new Image();
+			SpriteImages.src = "images/Sprite" + (i+1) + ".png";
+			SpriteSheets.push(SpriteImages);
+		}
+		return SpriteSheets;
 	}
 
-// GERE LES COLLISIONS AVEC LES BORDS
-function GestionDesBord(sprite)
-	{
-		if(carre.position.x > caneva.width - carre.carre.width)
-		{
-			carre.position.x = caneva.width - carre.carre.width;
-			carre.direction.x = - Math.random() - 0.5;
-			ControleurArmee(carre)
-		}
-		if(carre.position.x < 5)
-		{
-			carre.position.x = 5;
-			carre.direction.x = Math.random() + 0.5;
-			ControleurArmee(carre)
-		}
-		if(carre.position.y > caneva.height - carre.carre.height)
-		{
-			carre.position.y = caneva.height - carre.carre.height;
-			carre.direction.y = - Math.random() - 0.5;
-			ControleurArmee(carre)
-		}
-		if(carre.position.y < 5)
-		{
-			carre.position.y = 5;
-			carre.direction.y = Math.random() + 0.5;
-			ControleurArmee(carre)
-		}
-	}
-
-// GERE LES SOLDATS  !!!
-function ControleurArmee(sprite)
+function CreateRect(x,y,width,height,color)
 {
-	let nbSoldatsMax = 100,
-		color = ["red","blue","green","purple","yellow","pink","salmon","cyan", "magenta"],
-		indexRandom;
-	if(ArmeeDeCarres.length > nbSoldatsMax)
-	{
-		indexRandom = Math.trunc(Math.random() * 10);
-		carre.color = color[indexRandom];
-	}
-	else
-	{
-		ArmeeDeCarres.push(CreateCarre());
-	}
+		context.fillStyle = color;
+		context.fillRect(x,y,width,height);
 }
 //---------------------------------------------------------------------
 
 let caneva = {width :600 , height : 400},
-	ArmeeDeCarres = [];
-	ArmeeDeCarres.push(CreateCarre());
+	SpriteSheets = LoadSpriteSheets();
+	ArmeeDeSprite = [];
+	ArmeeDeSprite.push(CreateSprite(SpriteSheets[11],0, 0));
 
 
 
